298f0c2cfeb3326d04ea2914274fb636391cdf45
bfmqkqpnui <47877962@qq.com>
2019-03-26 17:49:13 +0800

    build

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import home from '@/views/home/home'
+import contact from '@/views/contact/contact'
+import goodsDetail from '@/views/goodsDetail/goodsDetail'
+
+Vue.use(Router)
+
+export default new Router({
+  base: '/h5/',
+  routes: [
+    {
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: home
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: contact
+    },
+    {
+      path: '/goodsDetail/:id',
+      name: 'goodsDetail',
+      component: goodsDetail
+    },
+  ]
+})
